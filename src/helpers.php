036e5b55a72a1cac61c1dbfb6a8eaca55e77bdb2
xutao <649223152@qq.com>
2018-05-16 10:39:48 +0800

    Add Helper function

---
+++ src/helpers.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xutao
+ * Date: 2018/5/16
+ * Time: 10:22
+ */
+
+if (function_exists('settings')) {
+    function settings($key, $default = null)
+    {
+        try {
+            $instance = app()->make('lara-setting');
+            $setting = $instance->get($key);
+            if (is_null($setting)) {
+                return $default;
+            }
+            return $setting;
+        } catch (Exception $exception) {
+            return $default;
+        }
+    }
+}
